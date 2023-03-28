33630279aef281770fbb2566f89d48e404a0bfdb
PierreLfvr <p.lefevre62100@hotmail.fr>
2023-03-28 18:18:18 -0400

    (API) Template WIP
    Ajout du template pour les grilles de salaire et les postes. Employées pas encore fait.

--- API Rest/src/db/template.js
+++ API Rest/src/db/template.js
@@ -1,1 +1,100 @@
-// TODO : Make database template
+const employeesTemplate = [
+
+]
+
+const salariesTemplate = [
+    {
+        jobId: 1,
+        level: 1,
+        grossIndex: 444,
+        increasedIndex: 390,
+        durationMonths: 18,
+        grossSalary: 1891.51
+    },
+    {
+        jobId: 1,
+        level: 2,
+        grossIndex: 484,
+        increasedIndex: 419,
+        durationMonths: 24,
+        grossSalary: 2032.16
+    },
+    {
+        jobId: 1,
+        level: 3,
+        grossIndex: 518,
+        increasedIndex: 445,
+        durationMonths: 24,
+        grossSalary: 2158.26
+    },
+    {
+        jobId: 1,
+        level: 4,
+        grossIndex: 565,
+        increasedIndex: 478,
+        durationMonths: 30,
+        grossSalary: 2318.31
+    },
+    {
+        jobId: 1,
+        level: 5,
+        grossIndex: 611,
+        increasedIndex: 513,
+        durationMonths: 36,
+        grossSalary: 2488.07
+    },
+    {
+        jobId: 1,
+        level: 6,
+        grossIndex: 646,
+        increasedIndex: 540,
+        durationMonths: 48,
+        grossSalary: 2619.02
+    },
+    {
+        jobId: 1,
+        level: 7,
+        grossIndex: 697,
+        increasedIndex: 578,
+        durationMonths: 48,
+        grossSalary: 2803.32
+    },
+    {
+        jobId: 1,
+        level: 8,
+        grossIndex: 739,
+        increasedIndex: 610,
+        durationMonths: 48,
+        grossSalary: 2958.52
+    },
+    {
+        jobId: 1,
+        level: 9,
+        grossIndex: 774,
+        increasedIndex: 637,
+        durationMonths: 48,
+        grossSalary: 3089.47
+    },
+    {
+        jobId: 1,
+        level: 10,
+        grossIndex: 821,
+        increasedIndex: 673,
+        grossSalary: 3264.07
+    }
+];
+
+const jobsTemplate = [
+    {
+        id: 1,
+        label: "Ingénieur hospitalier"
+    },
+    {
+        id: 2,
+        label: "Ingénieur hospitalier en chef de classe"
+    }
+]
+   
+module.exports = salariesTemplate;
+module.exports = employeesTemplate;
+module.exports = jobsTemplate;
