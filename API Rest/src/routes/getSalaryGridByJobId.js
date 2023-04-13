ab742346b4ce4953ba0173f2804a619c7665c146
PierreLfvr <p.lefevre62100@hotmail.fr>
2023-04-13 19:17:54 -0400

    (API) Trier getSalaryGrid by level

--- API Rest/src/routes/getSalaryGridByJobId.js
+++ API Rest/src/routes/getSalaryGridByJobId.js
@@ -3,8 +3,12 @@ const {Op} = require('sequelize');
 
 module.exports = (app) => {
 	app.get('/salaryGrid/:jobid',(req, res) => {
-		salaryGrid.findAll({where: 
-			{jobId: {[Op.eq]: req.params.jobid}}})
+		salaryGrid.findAll({
+			where: {
+				jobId: {[Op.eq]: req.params.jobid}
+			},
+			order: [['level', 'ASC']]
+		})
 		.then(e => {
 			const message = 'Salary grid fetched successfully.'
 			res.status(200).json({status:200, message, data: e })
